a76a60e97108b9bd1a9194064bcc8868b019a276
AlexTrejo92 <arq.alextrejo@gmail.com>
2023-04-15 12:11:33 -0600

    Incorporation of clickable buttons, tests with for loops

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,51 +9,65 @@ $(function wrapper() {
   function? How can DOM traversal be used to get the "hour-x" id of the
   time-block containing the button that was clicked? How might the id be
   useful when saving the description in local storage? */
-
-  var clickBtn = $('#btn');
-
+  var clickBtn = $('.saveBtn');
 clickBtn.on('click', function () {
   console.log('You clicked a button!');
-  
+  // code to store the event that the user wrote onto the local storage.
 });
 
-
-// var clickBtn = $('#hour-8').children()
-
-// console.log(clickBtn);
-
-
-
-
-  //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   var currentHour = dayjs().hour();
-  var scheduleHour = $('#hour-8')[0].attributes.value.textContent;
+  var timeBlock = $('.time-block').children('div');
+  console.log(timeBlock);
+  console.log (typeof timeBlock);
+  console.log(timeBlock[0]);
+  
+  var hourSchedule = 0
+  var scheduleHour = $('#hour-10')[hourSchedule].attributes.value.textContent;
   console.log(scheduleHour);
   console.log(typeof scheduleHour);
   console.log(currentHour);
   console.log(typeof currentHour);
 
-  scheduleHour = 8;
   // console.log(scheduleHour);
   // console.log(typeof scheduleHour);
 
+  /* Use a for loop to go through all of the time block hours and add remove
+  classes accordingly. This loop should run and compare each hour so that the
+  colors update when the current hour changes. */
+
+for (var i = 0; i < timeBlock.length; i++) {
   if (currentHour = scheduleHour ) {
     console.log('currentHour matches scheduleHour');
-    $('#hour-8').addClass('present');
-    $('#hour-8').removeClass('past')
-    $('#hour-8').removeClass('future')
+    $('.time-block').addClass('present');
+    $('.time-block').removeClass('past')
+    $('.time-block').removeClass('future')
   } else if (currentHour < scheduleHour) {
-    $('#hour-8').addClass('past')
-    $('#hour-8').removeClass('present')
-    $('#hour-8').removeClass('present')  
+    $('#hour-10').addClass('past')
+    $('#hour-10').removeClass('present')
+    $('#hour-10').removeClass('present')  
   } else {
-    $('#hour-8').addClass('future')
+    $('#hour-10').addClass('future')
   }
+  hourSchedule++
+}
+
+  // if (currentHour = scheduleHour ) {
+  //   console.log('currentHour matches scheduleHour');
+  //   $('#hour-10').addClass('present');
+  //   $('#hour-10').removeClass('past')
+  //   $('#hour-10').removeClass('future')
+  // } else if (currentHour < scheduleHour) {
+  //   $('#hour-10').addClass('past')
+  //   $('#hour-10').removeClass('present')
+  //   $('#hour-10').removeClass('present')  
+  // } else {
+  //   $('#hour-10').addClass('future')
+  // }
 
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
